86677b4ee05fc79021fd0282daa8a7550476e9fd
Maxime <maximeviard@hotmail.fr>
2025-11-06 10:32:13 +0000

    feat: add maxWithdraw/maxRedeem overrides with custody support

--- src/core/interfaces/IMultistrategyLockedVault.sol
+++ src/core/interfaces/IMultistrategyLockedVault.sol
@@ -119,4 +119,19 @@ interface IMultistrategyLockedVault is IMultistrategyVault {
      * @notice Cancels an active rage quit for the caller and frees any locked shares
      */
     function cancelRageQuit() external;
+    function getCustodyInfo(address user) external view returns (uint256 lockedShares, uint256 unlockTime);
+
+    /**
+     * @notice Get the amount of shares that can be transferred by a user
+     * @param user The address to check transferable shares for
+     * @return The amount of shares available for transfer (not locked in custody)
+     */
+    function getTransferableShares(address user) external view returns (uint256);
+
+    /**
+     * @notice Get the amount of shares available for rage quit initiation
+     * @param user The address to check rage quitable shares for
+     * @return The amount of shares available for initiating rage quit
+     */
+    function getRageQuitableShares(address user) external view returns (uint256);
 }
