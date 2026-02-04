27978a9c8008bd9b15ebff3515d1f2d45897cc7a
Ferit <ferit@cryptolab.net>
2026-02-04 15:35:46 +0000

    chore: clarify tokenized strategy variable

--- test/unit/core/tokenizedStrategy/Permit.t.sol
+++ test/unit/core/tokenizedStrategy/Permit.t.sol
@@ -9,7 +9,7 @@ import { MockStrategy as MockBaseStrategy } from "test/mocks/core/MockBaseStrate
 import { ITokenizedStrategy } from "src/core/interfaces/ITokenizedStrategy.sol";
 
 contract TokenizedStrategyPermitTest is Test {
-    MockBaseStrategy strategy;
+    MockBaseStrategy tokenizedStrategy;
     MockERC20 asset;
     MockYieldSource yieldSource;
     YieldDonatingTokenizedStrategy implementation;
@@ -32,7 +32,7 @@ contract TokenizedStrategyPermitTest is Test {
         asset = new MockERC20(18);
         yieldSource = new MockYieldSource(address(asset));
         implementation = new YieldDonatingTokenizedStrategy();
-        strategy = new MockBaseStrategy(address(asset), address(yieldSource), address(implementation));
+        tokenizedStrategy = new MockBaseStrategy(address(asset), address(yieldSource), address(implementation));
     }
 
     function _signWithDomain(
@@ -40,7 +40,7 @@ contract TokenizedStrategyPermitTest is Test {
         uint256 value,
         uint256 deadline
     ) internal view returns (uint8, bytes32, bytes32) {
-        uint256 nonce = ITokenizedStrategy(address(strategy)).nonces(owner);
+        uint256 nonce = ITokenizedStrategy(address(tokenizedStrategy)).nonces(owner);
         bytes32 structHash = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonce, deadline));
         bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
         return vm.sign(ownerPk, digest);
@@ -50,16 +50,16 @@ contract TokenizedStrategyPermitTest is Test {
         uint256 value = 1e18;
         uint256 deadline = block.timestamp + 1 days;
 
-        bytes32 nameHash = keccak256(bytes(ITokenizedStrategy(address(strategy)).name()));
+        bytes32 nameHash = keccak256(bytes(ITokenizedStrategy(address(tokenizedStrategy)).name()));
         bytes32 domainSeparator = keccak256(
-            abi.encode(EIP712DOMAIN_TYPEHASH, nameHash, VERSION_HASH, block.chainid, address(strategy))
+            abi.encode(EIP712DOMAIN_TYPEHASH, nameHash, VERSION_HASH, block.chainid, address(tokenizedStrategy))
         );
 
-        assertEq(ITokenizedStrategy(address(strategy)).DOMAIN_SEPARATOR(), domainSeparator);
+        assertEq(ITokenizedStrategy(address(tokenizedStrategy)).DOMAIN_SEPARATOR(), domainSeparator);
 
         (uint8 v, bytes32 r, bytes32 s) = _signWithDomain(domainSeparator, value, deadline);
-        ITokenizedStrategy(address(strategy)).permit(owner, spender, value, deadline, v, r, s);
+        ITokenizedStrategy(address(tokenizedStrategy)).permit(owner, spender, value, deadline, v, r, s);
 
-        assertEq(ITokenizedStrategy(address(strategy)).allowance(owner, spender), value);
+        assertEq(ITokenizedStrategy(address(tokenizedStrategy)).allowance(owner, spender), value);
     }
 }
